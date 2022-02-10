61fe8c1bd8f210b166cdf78c13342f207eacde2d
Mr.zhang <Mr.zhang@qq.com>
2022-02-10 18:57:16 +0800

    third commit

--- src/main/java/Demo/HelloGit.java
+++ src/main/java/Demo/HelloGit.java
@@ -4,5 +4,6 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("你好啊，我叫赛利亚");
         System.out.println("hello word");
+        System.out.println("hello github");
     }
 }
